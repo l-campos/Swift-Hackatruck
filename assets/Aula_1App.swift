4a124996d9c14cc06df9c4d4a49d8aea522c0c43
Lucas Fernandes da Silveira Campos <108770564+l-campos@users.noreply.github.com>
2025-06-03 15:15:17 -0300

    Add files via upload

---
+++ assets/Aula_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Aula_1App.swift
+//  Aula_1
+//
+//  Created by Turma02-19 on 30/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Aula_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
